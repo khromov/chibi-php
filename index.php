700fa8c87f3b1a1ec36ac419900fd37bdc24ab16
khromov <stanislav.khromov@gmail.com>
2013-08-05 07:30:04 +0200

    Moving config to applications folder

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 	/**
-	* Main file
+	* Main file for Chibi
 	*/
-	require('config.inc.php');
+	require('application/config.php');
 	
 	/** Composer autoloader DISABLED FOR NOW WHILE Toro gets a revamp **/
 	//require('vendor/autoload.php');
